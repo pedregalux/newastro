d7900826bdd6082f14aa8fb0100a64eec2b86367
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2024-10-05 13:25:23 -0300

    update

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.coerce.date(),
+    draft: z.boolean().optional()
+  }),
+});
+
+const work = defineCollection({
+  type: "content",
+  schema: z.object({
+    company: z.string(),
+    role: z.string(),
+    date: z.coerce.date(),
+    draft: z.boolean().optional()
+  }),
+});
+
+export const collections = { blog, work };

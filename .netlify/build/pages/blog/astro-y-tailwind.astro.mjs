f9f3a7d2cc10a2ab3c70abe2af1b2d63b690681c
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2025-03-16 21:53:39 -0300

    post

--- .netlify/build/pages/blog/astro-y-tailwind.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../../chunks/astro-y-tailwind_DKrgz9wl.mjs';
-export { renderers } from '../../renderers.mjs';
-
-const page = () => _page;
-
-export { page };
--- .netlify/build/pages/blog/rss-buena-idea-pero.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../../chunks/rss-buena-idea-pero_6z_Kwb-3.mjs';
-export { renderers } from '../../renderers.mjs';
-
-const page = () => _page;
-
-export { page };

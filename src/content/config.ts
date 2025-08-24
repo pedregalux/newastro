5ef5ac61f1545daa405cb3ff6140b57cf4147e2f
pedregalux <felipe.perry@gmail.com>
2025-08-23 22:15:34 -0400

    tags

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,25 +2,25 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/pages/blog" }),
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      pubDate: z.coerce.date(),
-      description: z.string(),
-      postimage: image().optional(),
-    }),
+    loader: glob({ pattern: "**/*.md", base: "./src/pages/blog" }),
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            pubDate: z.coerce.date(),
+            description: z.string(),
+            postimage: image().optional(),
+        }),
 });
 
-const work = defineCollection({
-  schema: z.object({
-    company: z.string(),
-    role: z.string(),
-    dateStart: z.coerce.date(),
-  }),
-});
+// const work = defineCollection({
+//   schema: z.object({
+//     company: z.string(),
+//     role: z.string(),
+//     dateStart: z.coerce.date(),
+//   }),
+// });
 
 export const collections = {
-  blog,
-  work,
+    blog,
+    // work,
 };
